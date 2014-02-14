af5ce44f53ff82475cb466b07f6798fbb4108fa3
HarisonP <harito92@abv.bg>
2014-02-14 11:59:30 +0200

    Implements view for hero_navigation and hero_creation(70%)

---
+++ public/js/hero_creation.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$('.sex').click(function () {
+		var sex = $(this).attr('id').split('_')[0];
+		$('#sex_field').val(sex);
+		$('#sex').hide();
+		$('#rasse').show();
+	});
+
+	$('.rasse').click(function () {
+		var rasse = $(this).attr('id').split('_')[0];
+		$('#rasse_field').val(sex);
+		$('#rasse').hide();
+		$('#class').show();
+	});
+
+	$('.class').click(function () {
+		var Class = $(this).attr('id').split('_')[0];
+		$('#class_field').val(sex);
+		$('#class').hide();
+		$('#hero_name').show();
+	});
+
+	$('#rasse a').click(function (){
+		$('#rasse').hide();
+		$('#sex').show();
+	});
+
+	$('#class a').click(function (){
+		$('#class').hide();
+		$('#rasse').show();
+	});
+
+	$('#hero_name a').click(function (){
+		$('#class').hide();
+		$('#rasse').show();
+	});
+})
